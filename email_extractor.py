631e2f891f972240ae5630ad104b8056d8e59918
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-12-01 19:25:41 -0600

    Fixed bug with dotted email usernames

--- email_extractor.py
+++ email_extractor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import re, sys
-email_pattern = re.compile('(\w+[.|\w]\w+@(\w+[.]\w+[.|\w+])\w+)')
+email_pattern = re.compile('([.|\w]+@(\w+[.]\w+[.|\w+])\w+)')
 data = sys.stdin.read()
 matches = re.findall(email_pattern, data)
 emails = [match[0] for match in matches]
