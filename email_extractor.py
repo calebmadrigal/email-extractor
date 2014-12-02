cc693877af7577dd7f9ba950c66c95d53530e423
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-12-01 19:11:03 -0600

    Unique

--- email_extractor.py
+++ email_extractor.py
@@ -3,4 +3,6 @@ import re, sys
 email_pattern = re.compile('(\w+[.|\w]\w+@(\w+[.]\w+[.|\w+])\w+)')
 data = sys.stdin.read()
 matches = re.findall(email_pattern, data)
-print ', '.join([match[0] for match in matches])
+emails = [match[0] for match in matches]
+unique_emails = list(set(emails))
+print ', '.join(unique_emails)
